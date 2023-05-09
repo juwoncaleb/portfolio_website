001119942b057d5a6a50057147c4e10b895741b6
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-05-09 12:02:25 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+     <link rel="icon" href="'https://ik.imagekit.io/juwoncaleb/lg3_Eh72ewKf5.webp?updatedAt=1683135944199'" />
+    </Head>
     <Component  {...pageProps} />
+    </>
   )
 }
 
